ab32a24e790c58ea8188efb9719b18dfbba70fa9
olexiychebotkevich <olexiychebotkevich@A33COMP4>
2019-04-08 16:43:00 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const PLACES=[
+  {name:'New-Yourk',zip:'10001'},
+  {name:'Washington',zip:'20001'},
+  {name:'Los Angeles',zip:'90023'},
+  {name:'Seattle',zip:'98101'}
+]
+class WeatherService extends Component{
+
+constructor (){
+  super();
+  this.state={weatherData:null};
+}
+  componentDidMount(){
+    const zip=this.props.zip;
+    const ApiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + zip + "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(ApiURL)
+    .then(res=>res.json())
+    .then(json=>{this.setState({weatherData:json})});
   }
+render(){
+  const weatherData=this.state.weatherData;
+  if(!weatherData) return <div>Loading...</div>
+  const weather=weatherData.weather[0];
+  const iconURL="http://api.openweathermap.org/img/w/"+ weather.icon +".png";
+  return(
+  // <div>JSON.stringify(weatherData)</div>
+  <div>
+    <h1>
+      {weather.main} in {weatherData.name}
+      <img src={iconURL} alt={weather.description}/>
+    </h1>
+    <p>Current temp {((weatherData.main.temp-32)/1.8).toFixed(1)}</p>
+    <p>Min temp {((weatherData.main.temp_min-32)/1.8).toFixed(1)}</p>
+    <p>Max temp {((weatherData.main.temp_max-32)/1.8).toFixed(1)}</p>
+    <p>Pressure {weatherData.main.pressure}mb</p>
+    <p>Wind speed {weatherData.wind.speed}m/sec</p>
+    <p>Humidity {weatherData.main.humidity}m/sec</p>
+  </div>
+  );
+
+}
+}
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      activePlace:0
+
+    };
+  
+
+  }
+  render() {
+    const activePlace=this.state.activePlace;
+    return (
+
+      <div className="App">
+      <WeatherService zip={PLACES[activePlace].zip} key={activePlace}/>
+       {PLACES.map((place,index)=>(
+         <button className={index%2===0?"button is-danger":"button is-link"} key={index} onClick={()=>{this.setState({activePlace:index})}}>
+           {/* //()=>{console.log(`Clicked ${index} button`)}}> */}
+          {place.name}
+         </button>
+        
+       ))}
+      </div>
+    )} 
+ 
 }
 
 export default App;
